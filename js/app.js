c4ff7edccbf648abb5039a556624ee628d049c9c
tonyrud <tonrudny@gmail.com>
2017-03-16 22:02:07 -0400

    api app completed

--- js/app.js
+++ js/app.js
@@ -1,31 +1,34 @@
-'use strict';
+'use strict'
 const express = require('express'),
   app = express(),
   port = 8080,
-  Twitter = require('twit'),
   config = require('./config.js'),
+  Twitter = require('twit'),
   twitter = new Twitter(config),
   path = require('path'),
-  moment = require('moment'),
-  displayCount = 5;
+  bodyParser = require('body-parser'),
+  displayCount = 5
 
-//create static serving of files
-app.use('/static', express.static(path.join(__dirname, '..\\public')));
+// create static serving of files
+app.use('/static', express.static(path.join(__dirname, '..\\public')))
 
-//setup pug templates
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
+// setup pug templates
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
-const stream = twitter.stream('statuses/user_timeline');
+const stream = twitter.stream('statuses/user_timeline')
 
 stream.on('tweet', function (tweet) {
+  debugger
   console.log(tweet)
 })
 
-function getTwitterData(next) {
-
-  //object to hold all of profile API data
+function getTwitterData (next) {
+  // object to hold all of profile API data
   const templateData = {
     tweets: [],
     profile: [],
@@ -33,27 +36,31 @@ function getTwitterData(next) {
     messages: []
   }
 
-  //get friends
+  // get friends
   twitter.get('friends/list', {
     screen_name: config.screen_name,
     count: displayCount
-  }, function (err, data, response) {
-    const followers = data.users;
+  }, function (err, data) {
+    if (!err && data) {
+      const followers = data.users
 
-    followers.forEach(follower => {
-      templateData.following.push({
-        follower_name: follower.name,
-        follower_screenName: follower.screen_name,
-        follower_img: follower.profile_image_url
-      });
-    });
-  });
+      followers.forEach(follower => {
+        templateData.following.push({
+          follower_name: follower.name,
+          follower_screenName: follower.screen_name,
+          follower_img: follower.profile_image_url
+        })
+      })
+    } else {
+      next(err)
+    }
+  })
 
-  //get messages
+  // get messages
   twitter.get('direct_messages', {
     screen_name: config.screen_name,
     count: 5
-  }, function (err, data, response) {
+  }, function (err, data) {
     if (!err && data) {
       data.forEach(msg => {
         templateData.messages.push({
@@ -67,22 +74,22 @@ function getTwitterData(next) {
     } else {
       next(err)
     }
-  });
+  })
 
-  //get tweets
+  // get tweets
   twitter.get('statuses/user_timeline', {
     screen_name: config.screen_name,
     count: displayCount
-  }, function (err, data, response) {
+  }, function (err, data) {
     if (!err && data) {
       for (let i in data) {
-        //destructure the basic data
+        // destructure the basic data
         const {
           text,
           favorite_count,
-          retweet_count,
-        } = data[i];
-        //create posts array for pug template
+          retweet_count
+        } = data[i]
+        // create posts array for pug template
         templateData.tweets.push({
           content: text,
           favCount: favorite_count,
@@ -93,39 +100,50 @@ function getTwitterData(next) {
           postTime: data[i].created_at.slice(0, 10)
         })
       }
-      //profile info
+      // profile info
       templateData.profile.push({
         banner: data[0].user.profile_banner_url,
         screenName: data[0].user.screen_name,
         profileImg: data[0].user.profile_image_url,
         following: data[0].user.friends_count
       })
-      //pass info arrays into next function call
-      next(err, templateData);
+      // pass info arrays into next function call
+      next(err, templateData)
     } else {
       next(err)
     }
   })
 }
 
-
-
-
 app.get('/', function (req, res) {
-  //make API call
+  // make API call
   getTwitterData(function (err, data) {
-    //check if any errors
+    // check if any errors
     if (!err && data) {
-      //pass data parameter into timeline object for pug rendering
+      // pass data parameter into timeline object for pug rendering
       res.render('index', {
         timeline: data
-      });
+      })
     } else {
-      res.send('err.message')
+      res.render('error', {
+        error: err.stack
+      })
     }
-  });
-});
+  })
+})
+
+// post tweet
+app.post('/', function (req, res) {
+  twitter.post('statuses/update', {
+    status: req.body.postTweet
+  }, function (err) {
+    if (!err) {
+    // redirect after posted
+      res.redirect('/')
+    }
+  })
+})
 
 app.listen(port, () => {
-  console.log(`server started on port: ${port}`);
-});
+  console.log(`server started on port: ${port}`)
+})
